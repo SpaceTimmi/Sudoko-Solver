bd07a87dbbd1a86e282205b4da0a46e8f3db9a3a
Timi Ombe <timmyombe@gmail.com>
2022-06-25 10:50:23 +0100

    Added units for testing for solvability

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 #Position is a list of size 81 and can contain any VAL
 
 
+# Data Defintions
+
 
 # Natural, Natural -> Natural
 # Convert a give row and column into a position
@@ -13,6 +15,10 @@ def rc_to_col(row, column):
 ALL_VALS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 B = False
 
+
+# Boards
+# Boards are represented with a lists that can contain any natural in ALL_Vals or B. It is an 81 element list representing all 81 positions
+# in  sudoku board
 BD1 =  [B, B, B, B, B, B, B, B, B,
         B, B, B, B, B, B, B, B, B,
         B, B, B, B, B, B, B, B, B,
@@ -77,6 +83,66 @@ BD4 =  [1, 2, 3, 4, 5, 6, 7, 8, B,
         B, B, B, B, B, B, B, B, 9] 
 
 
+# Rows is a list of lists that  refers to each row in a standard 9x9 sudoku board with 0 to 80 representing each position
+# 0 1 2 3 4 5 6 7 8 
+
+# Cols is a list of lists refers to each column in a standard 9x9 sudoku board with 0 to 80 representing each positon
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+# Boxes is a list of lists that refers to each individual 3x3 box in a 9x9 sudoku board
+# 0 1 2
+# 3 4 5 
+# 6 7 8
+
+ROWS = [[0, 1, 2, 3, 4, 5, 6, 7, 8],
+        [9, 10, 11, 12, 13, 14, 15, 16, 17],
+        [18, 19, 20, 21, 22, 23, 24, 25, 26],
+        [27, 28, 29, 30, 31, 32, 33, 34, 35],
+        [36, 37, 38, 39, 40, 41, 42, 43, 44],
+        [45, 46, 47, 48, 49, 50, 51, 52, 53],
+        [54, 55, 56, 57, 58, 59, 60, 61, 62],
+        [63, 64, 65, 66, 67, 68, 69, 70, 71],
+        [72, 73, 74, 75, 76, 77, 78, 79, 80]]
+ 
+COLS = [[0, 9, 18, 27, 36, 45, 54, 63, 72],
+        [1, 10, 19, 28, 37, 46, 55, 64, 73],
+        [2, 11, 20, 29, 38, 47, 56, 65, 74],
+        [3, 12, 21, 30, 39, 48, 57, 66, 75],
+        [4, 13, 22, 31, 40, 49, 58, 67, 76],
+        [5, 14, 23, 32, 41, 50, 59, 68, 77],
+        [6, 15, 24, 33, 42, 51, 60, 69, 78],
+        [7, 16, 25, 34, 43, 52, 61, 70, 79],
+        [8, 17, 26, 35, 44, 53, 62, 71, 80]]
+ 
+BOXES = [[0, 1, 2, 9, 10, 11, 18, 19, 20]
+         [3, 4, 5, 12, 13, 14, 21, 22, 23]
+         [6, 7, 8, 15, 16, 17, 24, 25, 26]
+         [27, 28, 29, 36, 37, 38, 45, 46, 47]
+         [30, 31, 32, 39, 40, 41, 48, 49, 50]
+         [33, 34, 35, 42, 43, 44, 51, 52, 53]
+         [54, 55, 56, 63, 64, 65, 72, 73, 74]
+         [57, 58, 59, 66, 67, 68, 75, 76, 77]
+         [60, 61, 62, 69, 70, 71, 78, 79, 80]]
+
+# Units represent all the possible locations where no duplicate number is supposed to be found. 
+UNITS = ROWS + COLS + BOXES
+
+
+
+# Functions
+
+# Board -> Board or Boolean
+# This function takes a board and returns a solved board if it is solvable otherwise it returns false
+ 
+ 
 
 
 
